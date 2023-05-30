4af1399e7ff52e4a38e6133dab640c7462375b9d
Hannah Kristine <hannahsoliao18@gmail.com>
2023-05-30 16:44:05 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const cards = document.querySelectorAll('.project__card, .about__card');
+
+cards.forEach((card, index) => {
+    card.addEventListener("click", () =>{
+        // get state
+        const state = Flip.getState(cards);
+
+        // add active class to the clicked card and add inactive to others
+        const isCardActive = card.classList.contains("active");
+        cards.forEach((otherCard, otherIdx) => {
+            otherCard.classList.remove("active");
+            otherCard.classList.remove("inactive");
+            if (!isCardActive && index !==otherIdx) {
+                otherCard.classList.add("inactive");
+            }
+        });
+        if (!isCardActive) card.classList.add("active");
+
+        Flip.from(state, {
+            duration: 1,
+            ease: "power1.out",
+            
+        });
+    });
+});
+
+
 let paths = document.querySelectorAll('path'),
 lines = document.querySelectorAll('line');
 
 fillSvgPath()
-fillSvgLine()
+
 document.addEventListener('scroll', fillSvgPath)
 
 function fillSvgPath() {
@@ -19,21 +46,5 @@ function fillSvgPath() {
         let drawLength = pathLength * scrollPercentage;
 
         path.style.strokeDashoffset = pathLength - drawLength;
-    }
-}
-function fillSvgLine() {
-    let scrollPercentage = (document.documentElement.scrollTop +document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-    for (var i = 0; i < lines.length; i++ ) {
-        let line = lines[i];
-        
-        let lineLength = line.getTotalLength();
-
-        line.style.strokeDasharray = lineLength;
-        line.style.strokeDashoffset = lineLength;
-
-        let drawLength = lineLength * scrollPercentage;
-
-        line.style.strokeDashoffset = lineLength - drawLength;
     }
 }
