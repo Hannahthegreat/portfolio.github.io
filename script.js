ae899bfe4941aa111c79d794c73462fa6c5d66ae
Hannah Kristine <hannahsoliao18@gmail.com>
2023-06-03 13:56:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-gsap.registerPlugin(Flip);
-
-const cards = document.querySelectorAll('.card'),
-infos = document.querySelectorAll('.info');
-
-cards.forEach((card, index) => {
-    card.addEventListener("click", () => {
-        const state = Flip.getState(cards);
-
-        const isCardActive = card.classList.contains("active");
-        cards.forEach((otherCard, otherIdx) => {
-            otherCard.classList.remove("active");
-            otherCard.classList.remove("inactive");
-            if(!isCardActive && index !== otherIdx) {
-                otherCard.classList.add("inactive");
-            }
-        });
-        if(!isCardActive) card.classList.add("active");
-
-        Flip.from(state, {
-            duration: 1,
-            ease: "expo.out",
-        });
-    });
-});
-
-
 let paths = document.querySelectorAll('path'),
 lines = document.querySelectorAll('line');
 
@@ -48,3 +21,25 @@ function fillSvgPath() {
         path.style.strokeDashoffset = pathLength - drawLength;
     }
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const cursor = document.querySelector('.cursor');
+document,addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
